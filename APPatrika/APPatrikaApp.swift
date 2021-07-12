3c5bae7928591af05ab19842dacddf4506703e2e
Parth Patel <parthpatel1105@gmail.com>
2021-07-12 18:19:42 -0400

    Initial Commit

---
+++ APPatrika/APPatrikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPatrikaApp.swift
+//  APPatrika
+//
+//  Created by Parth Patel on 12/07/21.
+//
+
+import SwiftUI
+
+@main
+struct APPatrikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APPatrika/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APPatrika
+//
+//  Created by Parth Patel on 12/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
