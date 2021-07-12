895a47f577e99463044d8014bcece8d438d8ea5c
Parth Patel <parthpatel1105@gmail.com>
2021-07-12 18:22:28 -0400

    Setup Project
    Setup Project

---
+++ Pods/Target Support Files/Pods-APPatrika-APPatrikaUITests/Pods-APPatrika-APPatrikaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_APPatrika_APPatrikaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_APPatrika_APPatrikaUITests
+@end
---
+++ Pods/Target Support Files/Pods-APPatrika/Pods-APPatrika-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_APPatrika : NSObject
+@end
+@implementation PodsDummy_Pods_APPatrika
+@end
